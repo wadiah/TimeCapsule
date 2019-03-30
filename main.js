e83205870174b88f148b2474fbcf1b3a170116f7
wadiah <wadiah.tm@gmail.com>
2019-03-30 09:58:32 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById("homepage").style.margin = "75px 100px 100px 75px";
+document.getElementById("homepage").style.fontFamily = "Arial, sans-serif";
+
+document.getElementById("x").style.color = "black";
