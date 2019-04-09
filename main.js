b4145a6a8939062d78a78ba4b5884b4e97454b7d
wadiah <wadiah.tm@gmail.com>
2019-04-09 15:28:09 -0400

    updated homepage

--- main.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("homepage").style.margin = "75px 100px 100px 75px";
-document.getElementById("homepage").style.fontFamily = "Arial, sans-serif";
-
-document.getElementById("x").style.color = "black";
